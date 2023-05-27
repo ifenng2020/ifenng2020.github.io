f933fb292d20741cbe37c4013db0f1e4ff080380
ifenng2020 <ifenng2020@users.noreply.github.com>
2023-05-27 14:52:10 +0000

    deploy: ifenng2020/blog@ca12e8eb7a6df67d8e74f1892eee170e830652a5

--- js/totop.js
+++ js/totop.js
@@ -10,3 +10,11 @@ $("#rocket").click(function() {
     });
     return false;
 });
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0429b703a9f0f6ebf90fece1ec649377";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
